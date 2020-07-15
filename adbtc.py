52554d4f9441aa8f3a7da109533fd489f8ebaad1
alan7s <alan7@outlook.com>
2020-07-14 22:46:24 -0300

    Update adbtc.py

--- adbtc.py
+++ adbtc.py
@@ -6,17 +6,21 @@ email='your_email'
 senha='your_password'
 
 driver.get("https://adbtc.top/index/enter/")
-time.sleep(30) #30s para resolver o hCaptcha
+time.sleep(30) #30s for you solve hCaptcha
 driver.find_element_by_id('email').send_keys(email)
 driver.find_element_by_id('password').send_keys(senha)
 driver.find_element_by_css_selector('.btn.light-blue.darken-4').click()
 time.sleep(10)
 driver.get("https://adbtc.top/surf/rotator")
-time.sleep(45) #45s para resolver o hCaptcha
+time.sleep(45) #45s for you solve hCaptcha
 while True:
     try:
         driver.find_element_by_css_selector('.pulse.animated.valign-wrapper.btn').click()
         driver.switch_to.window(driver.window_handles[0])
         time.sleep(45)
+        driver.switch_to.window(driver.window_handles[1])
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
     except:
-        print("error")
+        print("Please solve the captchar! Or something is wrong...")
+        time.sleep(10)
