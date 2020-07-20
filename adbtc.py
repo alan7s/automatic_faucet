57f1b19cecdee473eba0600652c6eca63be66857
alan7s <alan7@outlook.com>
2020-07-20 18:17:10 -0300

    Update adbtc.py

--- adbtc.py
+++ adbtc.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+driver = webdriver.Chrome(options=chrome_options)
+
 email='your_email'
 senha='your_password'
 
 driver.get("https://adbtc.top/index/enter/")
-time.sleep(30) #30s for you solve hCaptcha
+time.sleep(40) #40s for you solve hCaptcha
 driver.find_element_by_id('email').send_keys(email)
 driver.find_element_by_id('password').send_keys(senha)
 driver.find_element_by_css_selector('.btn.light-blue.darken-4').click()
@@ -29,4 +33,11 @@ while True:
         driver.switch_to.window(driver.window_handles[0])
     except:
         print("Please solve the captchar! Or something is wrong...")
+        try:
+            teste=driver.find_element_by_css_selector('.open.btn.green')
+            print("sleeping")
+            time.sleep(120)
+            driver.get("https://adbtc.top/surf/rotator")
+        except:
+            print('awake')
         time.sleep(10)
