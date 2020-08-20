cec9532966bccf80dc18b4501d40e616bc16ade9
alan7s <alan7@outlook.com>
2020-08-20 10:56:47 -0300

    Update adbtc.py

--- adbtc.py
+++ adbtc.py
@@ -3,7 +3,7 @@ import time
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+chrome_options.add_argument('--user-agent=Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36')
 driver = webdriver.Chrome(options=chrome_options)
 
 email='your_email'
@@ -11,11 +11,13 @@ senha='your_password'
 
 driver.get("https://adbtc.top/index/enter/")
 time.sleep(40) #40s for you solve hCaptcha
-driver.find_element_by_id('email').send_keys(email)
+driver.find_element_by_id('thefirstfield').send_keys(email)
 driver.find_element_by_id('password').send_keys(senha)
 driver.find_element_by_css_selector('.btn.light-blue.darken-4').click()
 time.sleep(10)
-driver.get("https://adbtc.top/surf/rotator")
+driver.get("https://adbtc.top/index/earn") #https://adbtc.top/surf/rotator
+time.sleep(6)
+driver.find_element_by_css_selector('.btn.btn-large.yellow.black-text').click()
 time.sleep(45) #45s for you solve hCaptcha
 while True:
     try:
@@ -36,8 +38,10 @@ while True:
         try:
             teste=driver.find_element_by_css_selector('.open.btn.green')
             print("sleeping")
-            time.sleep(120)
-            driver.get("https://adbtc.top/surf/rotator")
+            time.sleep(300)
+            driver.get("https://adbtc.top/index/earn")  # https://adbtc.top/surf/rotator
+            time.sleep(6)
+            driver.find_element_by_css_selector('.btn.btn-large.yellow.black-text').click()
         except:
-            print('awake')
+            print('wake up')
         time.sleep(10)
